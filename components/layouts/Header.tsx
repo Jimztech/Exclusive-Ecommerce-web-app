8bab37211ac0eab525d54d4f7598209db859154c
jimztech <ibezimjames20@gmail.com>
2025-11-27 23:37:51 +0100

    working on nav bar and home page

---
+++ components/layouts/Header.tsx
@@ -0,0 +1,12 @@
+import NavBar from "./Navbar"
+export default function Header() {
+    return (
+        <div className="">
+            <NavBar />
+
+            <section className="pt-30">
+                <h1>Hero</h1>
+            </section>
+        </div>
+    )
+}
